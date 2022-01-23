e9a234cc316afca922ec7f15410704f401527416
Jordan Coleman <jzc0197@auburn.edu>
2022-01-23 12:03:24 -0600

    trying to implement delay function using chrono library

--- I2C_Test/delay.cpp
+++ I2C_Test/delay.cpp
@@ -8,16 +8,17 @@ using namespace std::chrono;
 void delay(long millis) {
 
     long finished = millis;
-    long elapsed = 0;
-    std::chrono::time_point<std::chrono::steady_clock> t1 = std::chrono::steady_clock::now();
-    std::chrono::time_point<std::chrono::steady_clock> t2;
+    long elapsed;
+    time_point<system_clock> t1 = system_clock::now();
+    time_point<system_clock> t2;
     do {
 
-        t2 = std::chrono::steady_clock::now();
-        elapsed = std::chrono::duration_cast<milliseconds>(t2 - t1).count();
+        t2 = system_clock::now();
+        elapsed = duration_cast<milliseconds>(t2 - t1).count();
 
 
     }
     while(elapsed < finished);
 
 }
+
