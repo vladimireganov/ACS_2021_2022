3f14d6c7f4e12035b83e8acc65152cf2b2e5718c
Kazybek Mizam <kazymizam@mail.ru>
2022-03-02 12:35:47 -0600

    add new launch file

---
+++ FebLaunch_F/Data.h
@@ -0,0 +1,56 @@
+// class for data storage
+// currently stores raw data
+// TO DO
+// add raw data
+// add functions for obtaining processed data
+
+// add includes for sensors
+
+
+class Data
+{
+private:
+    /* data */
+
+public:
+    int iterator; // number of iteration
+
+    int iteration_time; // time
+    float pressure;
+    float linear_acceleration_x;
+    float linear_acceleration_y;
+    float linear_acceleration_z;
+    float gyroscope_x;
+    float gyroscope_y;
+    float gyroscope_z;
+    float acceleration_x;
+    float acceleration_y;
+    float acceleration_z;
+    float gravity_x;
+    float gravity_y;
+    float gravity_z;
+
+    // section for processed data
+    //float altitude;
+
+    Data(/* args */);
+    ~Data();
+    void get_data();
+    void process_data();
+};
+
+Data::Data(/* args */)
+{
+}
+
+Data::~Data()
+{
+}
+
+void Data::get_data(){
+    // reading all the data
+}
+
+void Data::process_data(){
+    // getting processed data
+}
