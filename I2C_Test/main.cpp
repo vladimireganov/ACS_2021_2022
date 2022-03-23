0223f293660f70fa227341de2614f77c06168900
Jordan Coleman <jzc0197@auburn.edu>
2022-03-23 15:33:31 -0500

    new folder for testing BMP disable function

---
+++ I2C_Test/main.cpp
@@ -0,0 +1,44 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <fcntl.h>
+#include <unistd.h>
+#include "PWM.h"
+#include "LED.h"
+
+
+
+
+using namespace std;
+
+
+
+
+int main() {
+
+
+    //////////          SERVO, BUZZER/LED INIT AND BMP388 DISABLE  /////////////
+
+    if (gpioInitialise() < 0) {
+        cout << "GPIO Init Fail\n";
+        while (1);
+    }
+    disBMP();
+
+
+
+
+
+
+
+
+            //////////////////  closing everything   /////////////////////////
+
+
+
+            gpioTerminate();
+            close(RPI_I2C_BUS);
+
+            return 0;
+        }
