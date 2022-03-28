1771f655e78f89b1e7b1b46f2906770da81a5c29
Kazybek Mizam <kazymizam@mail.ru>
2022-03-28 15:56:10 -0500

    update file write

--- tests and sources/Senior Design Fair/File_write.h
+++ tests and sources/Senior Design Fair/File_write.h
@@ -16,7 +16,7 @@ private:
     //string data_test; //testing git push
     string name_of_the_file;
     string log_file_name;
-    ofstream data_file;
+    ofstream flight_data_file;
     ofstream log_file;
 
 public:
@@ -49,45 +49,45 @@ void File_write::connect_data(Data& data){
 }
 
 void File_write::create_table_names(){ //prints name of the columns in the csv file
-    data_file.open(name_of_the_file);
+    flight_data_file.open(name_of_the_file);
 
-    data_file << "iterator,";
-    data_file << "iteration_time,";
-    data_file << "pressure,";
-    data_file << "linear_acceleration_x,";
-    data_file << "linear_acceleration_y,";
-    data_file << "linear_acceleration_z,";
-    data_file << "gyroscope_x,";
-    data_file << "gyroscope_y,";
-    data_file << "gyroscope_z,";
-    data_file << "acceleration_x,";
-    data_file << "acceleration_y,";
-    data_file << "acceleration_z,";
-    data_file << "gravity_x,";
-    data_file << "gravity_y,";
-    data_file << "gravity_z\n";
-    data_file.flush();
+    flight_data_file << "iterator,";
+    flight_data_file << "iteration_time,";
+    flight_data_file << "pressure,";
+    flight_data_file << "linear_acceleration_x,";
+    flight_data_file << "linear_acceleration_y,";
+    flight_data_file << "linear_acceleration_z,";
+    flight_data_file << "gyroscope_x,";
+    flight_data_file << "gyroscope_y,";
+    flight_data_file << "gyroscope_z,";
+    flight_data_file << "acceleration_x,";
+    flight_data_file << "acceleration_y,";
+    flight_data_file << "acceleration_z,";
+    flight_data_file << "gravity_x,";
+    flight_data_file << "gravity_y,";
+    flight_data_file << "gravity_z\n";
+    flight_data_file.flush();
 
-    data_file.close();
+    flight_data_file.close();
 }
 
 
 void File_write::create_log_file(){ // creates log file
     log_file.open(log_file_name);
     log_file << "Created log file\n";
-    data_file.flush();
+    flight_data_file.flush();
     log_file.close();
 }
 
 void File_write::log_error(string msg){ // logs error to the log
     log_file.open(log_file_name);
     log_file << msg + "\n";
-    data_file.flush();
+    flight_data_file.flush();
     log_file.close();
 }
 
 void File_write::save_data(){ // prints data into the file
-    data_file.open(name_of_the_file);
+    flight_data_file.open(name_of_the_file);
     /*
     int iterator; // number of iteration
 
@@ -108,31 +108,31 @@ void File_write::save_data(){ // prints data into the file
 
     */
     // raw data
-    data_file << flight_data->iterator << ",";
-    data_file << flight_data->iteration_time << ",";
-    data_file << flight_data->pressure << ",";
-    data_file << flight_data->linear_acceleration_x << ",";
-    data_file << flight_data->linear_acceleration_y << ",";
-    data_file << flight_data->linear_acceleration_z << ",";
-    data_file << flight_data->gyroscope_x << ",";
-    data_file << flight_data->gyroscope_y << ",";
-    data_file << flight_data->gyroscope_z << ",";
-    data_file << flight_data->acceleration_x << ",";
-    data_file << flight_data->acceleration_y << ",";
-    data_file << flight_data->acceleration_z << ",";
-    data_file << flight_data->gravity_x << ",";
-    data_file << flight_data->gravity_y << ",";
-    data_file << flight_data->gravity_z << "\n";
+    flight_data_file << flight_data->iterator << ",";
+    flight_data_file << flight_data->iteration_time << ",";
+    flight_data_file << flight_data->pressure << ",";
+    flight_data_file << flight_data->linear_acceleration_x << ",";
+    flight_data_file << flight_data->linear_acceleration_y << ",";
+    flight_data_file << flight_data->linear_acceleration_z << ",";
+    flight_data_file << flight_data->gyroscope_x << ",";
+    flight_data_file << flight_data->gyroscope_y << ",";
+    flight_data_file << flight_data->gyroscope_z << ",";
+    flight_data_file << flight_data->acceleration_x << ",";
+    flight_data_file << flight_data->acceleration_y << ",";
+    flight_data_file << flight_data->acceleration_z << ",";
+    flight_data_file << flight_data->gravity_x << ",";
+    flight_data_file << flight_data->gravity_y << ",";
+    flight_data_file << flight_data->gravity_z << "\n";
 
     // processed data
 
-    data_file.flush();
-    data_file.close();
+    flight_data_file.flush();
+    flight_data_file.close();
 }
 
 void File_write::close_files(){
-    data_file.flush();
+    flight_data_file.flush();
     log_file.flush();
-    data_file.close();
+    flight_data_file.close();
     log_file.close();
 }
