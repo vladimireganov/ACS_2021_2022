7b8d712344d5226a6042df692ee48f27cbe94551
1kzpro <kazymizam@mail.ru>
2022-04-09 20:21:00 -0500

    add time into file manager logs

--- launches/AprilLaunchAnalysis/FileManager.h
+++ launches/AprilLaunchAnalysis/FileManager.h
@@ -20,6 +20,8 @@
 #include <ctime>
 #include <sstream>
 
+#include "ArduinoTimer.h"
+
 #define FILE_MANAGER_BASE_DIRECTORY ""
 #define FILE_MANAGER_SETTINGS_FILE_NAME "filemanager_settings.txt"
 #define FILE_MANAGER_LOG_FILE_NAME "filemanager_log.txt"
@@ -44,7 +46,7 @@ class FileManager {
 
         settingsFile.open(baseDirectory + "/" + settingsFileName, std::ios::in);
         if (!settingsFile.is_open()) {
-            std::cout << "Error opening FileManager settings file" << std::endl;
+            std::cout << seconds() << "Error opening FileManager settings file" << std::endl;
             correctlyInitialized = false;
             return;
         }
@@ -58,7 +60,7 @@ class FileManager {
 
         settingsFile.open(baseDirectory + "/" + settingsFileName, std::ios::out);
         if (!settingsFile.is_open()) {
-            std::cout << "Error opening FileManager settings file" << std::endl;
+            std::cout << seconds() << "Error opening FileManager settings file" << std::endl;
             correctlyInitialized = false;
             return;
         }
@@ -87,8 +89,8 @@ public:
         newFile->open(baseDirectory + "/" + fileName + "_" + uniqueFileID);
 
         if (!newFile->is_open()) {
-            logFile << "Error opening " << fileName << "_" << uniqueFileID << " file!" << std::endl;
-            std::cout << "Error opening " << fileName << "_" << uniqueFileID << " file!" << std::endl;
+            logFile << seconds() << "Error opening " << fileName << "_" << uniqueFileID << " file!" << std::endl;
+            std::cout << seconds() << "Error opening " << fileName << "_" << uniqueFileID << " file!" << std::endl;
         }
         else {
             auto t = std::time(nullptr);
@@ -97,8 +99,8 @@ public:
             oss << std::put_time(&tm, "%d-%m-%Y %H-%M-%S");
             std::string dateTimeString = oss.str();
 
-            logFile << "Opened " << fileName << "_" << uniqueFileID << " at " << dateTimeString << std::endl;
-            std::cout << "Opened " << fileName << "_" << uniqueFileID << " at " << dateTimeString << std::endl;
+            logFile << seconds() << "Opened " << fileName << "_" << uniqueFileID << " at " << dateTimeString << std::endl;
+            std::cout << seconds() << "Opened " << fileName << "_" << uniqueFileID << " at " << dateTimeString << std::endl;
         }
         
         return newFile;
