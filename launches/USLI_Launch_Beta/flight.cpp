7f5dae9529508d57645c90ba57b4fdb6c6d87272
Kazybek Mizam <kazymizam@mail.ru>
2022-04-14 18:02:46 -0500

    add hardware manager support

--- launches/USLI_Launch_Beta/flight.cpp
+++ launches/USLI_Launch_Beta/flight.cpp
@@ -3,13 +3,7 @@
 #include "FileManager.h"
 #include "LogManager.h"
 #include "DataManager.h"
-
-// #include "hardware/MS5607.h"
-// #include "hardware/DFRobot_BMX160.h"
-// #include "hardware/PWM.h"
-// #include "hardware/SerialLinux.h"
-// #include "hardware/LED.h"
-
+#include "HardwareManager.h"
 
 #define DATA_FILENAME "data.csv"
 #define LOG_FILENAME "log.txt"
@@ -36,10 +30,13 @@ int main() {
     auto dataFile = fileManager.createFile(DATA_FILENAME);
     DataManager dataManager = DataManager(dataFile);
 
+    HardwareManager hardwareManager = HardwareManager();
+
     /* Setup */
     if (!fileManager.start()) return 1;
     if (!logManager.start()) return 1;
     if (!dataManager.start()) return 1;
+    if (!hardwareManager.start()) return 1;
 
     /* Run */
     do {
@@ -50,4 +47,5 @@ int main() {
     /* Stop & Cleanup */
     logManager.stop();
     dataManager.stop();
+    hardwareManager.stop();
 }
