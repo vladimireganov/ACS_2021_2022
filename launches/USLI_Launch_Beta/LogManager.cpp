7201b0d36ab5e8b441f67067e98fdc70c14d0c9d
Kazybek Mizam <kazymizam@mail.ru>
2022-04-15 15:03:01 -0500

    improve LogManager

--- launches/USLI_Launch_Beta/LogManager.cpp
+++ launches/USLI_Launch_Beta/LogManager.cpp
@@ -8,20 +8,25 @@ LogManager::~LogManager() {}
 
 void LogManager::body(std::string message) {
     *logFile << "\t";
-    *logFile << seconds();
+    *logFile << millis();
     *logFile << "\t";
     *logFile << message.c_str();
     *logFile << "\n";
+
+    logFile->flush();
 }
 
 void LogManager::info(std::string message) {
     *logFile << "INFO";
+    this->body(message);
 }
 void LogManager::warning(std::string message) {
     *logFile << "WARNING";
+    this->body(message);
 }
 void LogManager::error(std::string message) {
     *logFile << "ERROR";
+    this->body(message);
 }
 
 bool LogManager::start() {
