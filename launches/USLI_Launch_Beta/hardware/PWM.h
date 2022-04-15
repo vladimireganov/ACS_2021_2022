f708dfa109ae261e75351440c36572009f2672a0
Kazybek Mizam <kazymizam@mail.ru>
2022-04-15 15:02:28 -0500

    update PWM library

--- launches/USLI_Launch_Beta/hardware/PWM.h
+++ launches/USLI_Launch_Beta/hardware/PWM.h
@@ -1,3 +1,6 @@
+#ifndef _PMW_H_
+#define _PMW_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,6 +12,8 @@
 #define dCycle0 135000		//13.5% is zero point 0degrees, 80% is max, 270degrees
 #define freq 333		//135 355 575 795
 
-void ServoOff(void);
+void ServoOff();
 void SetAngle(int angle);
 void servoSweep();
+
+#endif
