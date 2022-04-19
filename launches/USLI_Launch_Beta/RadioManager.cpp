5be7bc90cfbc0d16527f464814d222aff5dbf75c
Kazybek Mizam <kazymizam@mail.ru>
2022-04-18 20:09:15 -0500

    fix radio processing bug

--- launches/USLI_Launch_Beta/RadioManager.cpp
+++ launches/USLI_Launch_Beta/RadioManager.cpp
@@ -37,6 +37,7 @@ void RadioManager::collect() {
             std::string infoMessage = "[RadioManager] Received request " + request;
             std::cout << millis() << "\t[RadioManager] Received request " << request << "\n";
             logManager->info(infoMessage);
+            Serial.println(infoMessage);
         }
         else {
             request += buffer[i];
@@ -51,17 +52,39 @@ void RadioManager::handle() {
     for (auto i = decodedRequests.begin(); i != decodedRequests.end(); ++i) {
         if (*i == "War Eagle") {
             configuration->arm = !configuration->arm;
+            if (configuration->arm) {
+                std::cout << millis() << "\t[RadioManager] System is ARMED\n";
+                logManager->info("[RadioManager] System is ARMED");
+            }
+            else {
+                std::cout << millis() << "\t[RadioManager] System is DISARMED\n";
+                logManager->info("[RadioManager] System is DISARMED");
+            }
         }
         else if (*i == "Servo Sweep") {
             configuration->run_servo_sweep = !configuration->run_servo_sweep;
+            std::cout << millis() << "\t[RadioManager] Processed Servo Sweep request\n";
+            logManager->info("[RadioManager] Processed Servo Sweep request");
         }
         else if (*i == "Real Time") {
             configuration->send_real_time_data = !configuration->send_real_time_data;
+            if (configuration->send_real_time_data) {
+                std::cout << millis() << "\t[RadioManager] Real Time Data Enabled\n";
+                logManager->info("[RadioManager] Real Time Data Enabled");
+            }
+            else {
+                std::cout << millis() << "\t[RadioManager] Real Time Data Disabled\n";
+                logManager->info("[RadioManager] Real Time Data Disabled");
+            }
         }
         else if (*i == "Shutdown") {
             configuration->shutdown = !configuration->shutdown;
+            std::cout << millis() << "\t[RadioManager] Processed Shutdown request\n";
+            logManager->info("[RadioManager] Processed Shutdown request");
         }
     }
+
+    decodedRequests.clear();
 }
 
 void RadioManager::run() {
